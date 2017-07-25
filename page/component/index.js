4c9506a182383053eb72772a7426e90c4e3b3add
李林 <ww1095@163.com>
2017-07-25 18:42:54 +0800

    列表渲染 列表下标记录

---
+++ page/component/index.js
@@ -0,0 +1,56 @@
+Page({
+  data: {
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+      }, {
+        id: 'nav',
+        name: '导航',
+        open: false,
+        pages: ['navigator']
+      }, {
+        id: 'media',
+        name: '媒体组件',
+        open: false,
+        pages: ['image', 'audio', 'video']
+      }, {
+        id: 'map',
+        name: '地图',
+        pages: ['map']
+      }, {
+        id: 'canvas',
+        name: '画布',
+        pages: ['canvas']
+      }
+    ]
+  },
+  // 点击列表
+  clickListItem: function (e) {
+
+    var target  = e.currentTarget;
+
+    var index   = target.dataset.index; // 列表下标
+    var id      = target.id;            // 列表唯一标识
+    
+    var list    = this.data.list;       // 列表数组
+    var name    = list[index].name;
+
+    wx.showToast({
+      title: name,
+    })
+  }
+})
+
